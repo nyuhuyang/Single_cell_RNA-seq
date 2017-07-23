f9a9afb9c742aa85cda5c952d65cb34faab384c1
Yang Hu <nyuhuyang@gmail.com>
2017-07-22 23:50:44 -0400

    Add files via upload

--- R/4.simpleSingleCellworkflow.R
+++ R/4.simpleSingleCellworkflow.R
@@ -17,24 +17,11 @@ list.of.cran.packages <- c("cowplot")
 new.packages <- list.of.cran.packages[!(list.of.cran.packages %in% installed.packages()[,"Package"])]
 if(length(new.packages)) install.packages(new.packages)
 
-source("http://bioconductor.org/workflows.R")
-workflowInstall("simpleSingleCell")
+#source("http://bioconductor.org/workflows.R")
+#workflowInstall("simpleSingleCell")
 
 source("https://bioconductor.org/biocLite.R")
-list.of.bio.packages <- c("minqa", "nloptr", "lme4", "SparseM", 
-                           "MatrixModels", "modeltools", "prettyunits", 
-                           "pbkrtest", "quantreg", "flexmix", "prabclus", 
-                           "diptest", "trimcluster", "progress", "lmtest", 
-                           "xts", "Rsamtools", "GenomicAlignments", "e1071",
-                           "pls", "car", "cvTools", "fpc", "GGally", 
-                           "kernlab", "mclust", "sROC", "sp", "vcd", 
-                           "laeken", "TTR", "rtracklayer", "bookdown", 
-                           "sgeostat", "robCompositions", "FNN", "VIM", 
-                           "smoother", "scatterplot3d", "R.oo", "R.methodsS3",
-                           "zoo", "statmod", "graph", "GenomicFeatures", 
-                           "BiocStyle", "mvoutlier", "destiny", "R.utils", 
-                           "readxl", "scran", "RBGL","scran", 
-                           "TxDb.Mmusculus.UCSC.mm10.ensGene")
+list.of.bio.packages <- c("scater","org.Mm.eg.db")
 new.packages <- list.of.bio.packages[!(list.of.bio.packages %in% installed.packages()[,"Package"])]
 if(length(new.packages)) biocLite(new.packages)
 library(easypackages)
@@ -67,15 +54,14 @@ sce <- newSCESet(countData = exprs(gbm), phenoData = pd,
 sce
 dim(sce)
 #We identify the rows corresponding to ERCC spike-ins and mitochondrial genes. 
-is.spike <- grepl("^ERCC", fData(sce)$Gene)
+is.spike <- grepl("^ERCC", fData(sce)$Gene) # External RNA Controls Consortium
 is.mito <- grepl("^mt-", fData(sce)$Gene)
 #calculate quality control metrics such as the total number of counts 
 #or the proportion of counts in mitochondrial genes or spike-in transcripts.
 sce <- calculateQCMetrics(sce, feature_controls=list(ERCC=is.spike, Mt=is.mito))
 head(pData(sce),1)
 head(fData(sce),1)
-library(scran)  #?
-setSpike(sce) <- "ERCC" #?
+setSpike(sce) <- "ERCC"
 
 ########################################################################
 #
@@ -86,12 +72,76 @@ setSpike(sce) <- "ERCC" #?
 # expressed features (genes per cell) = the number of features with non-zero counts
 #  Cells with small library sizes and few expressed genes are like to be of poor quality.
 par(mfrow=c(1,2))
-hist(sce$total_counts/1e6, xlab="Library sizes (millions)", main="", 
-     breaks=20, col="grey80", ylab="Number of cells")
-hist(sce$total_features, xlab="Number of expressed genes", main="", 
-     breaks=20, col="grey80", ylab="Number of cells")
-qplot(counts_per_cell, data=sce$total_counts/1e6, geom="density")
-qplot(counts_per_cell, data=pData(gbm), geom="density")+coord_cartesian(xlim = c(0, 30000))
+hist(sce1$total_counts/1e3, xlab="Library sizes (thousands)", main="", 
+     breaks=30, col="grey80", ylab="Number of cells")
+hist(sce1$total_features, xlab="Number of expressed genes", main="", 
+     breaks=30, col="grey80", ylab="Number of cells")
+#remove cells with log-library sizes that are more than 3 median absolute deviations (MADs)
+#below the median log-library size.
+libsize.drop <- isOutlier(sce$total_counts, nmads=3, type="lower", log=TRUE)
+table(libsize.drop)
+
+feature.drop <- isOutlier(sce$total_features, nmads=3, type="lower", log=TRUE)
+table(feature.drop)
+
+sce <- sce[,!(libsize.drop | feature.drop )]#subsetting by column
+
+
+#Removal of a substantial proportion of cells (> 10%) may be indicative of an overall issue with data quality
+dim(sce)
+data.frame(ByLibSize=sum(libsize.drop), ByFeature=sum(feature.drop),
+           Remaining=ncol(sce))
+
+
+#-------QC on mt- & ERCC (Alternative)-----------------------------------------
+par(mfrow=c(1,2))
+#High mitochondrial proportion indicates poor-quality of cells.
+hist(sce$pct_counts_feature_controls_Mt, xlab="Mitochondrial proportion (%)", 
+     ylab="Number of cells", breaks=20, main="", col="grey80")
+#High proportion spike-in RNA indicates loss of endogenous RNA in low-quality cells.
+hist(sce$pct_counts_feature_controls_ERCC, xlab="ERCC proportion (%)", 
+     ylab="Number of cells", breaks=20, main="", col="grey80")
+
+#use the MAD-based definition of outliers to remove putative low-quality cells from the dataset
+mito.drop <- isOutlier(sce$pct_counts_feature_controls_Mt, nmads=3, type="higher")
+spike.drop <- isOutlier(sce$pct_counts_feature_controls_ERCC, nmads=3, type="higher")
+table(mito.drop)
+table(spike.drop)
+
+sce <- sce[,!(libsize.drop | feature.drop | mito.drop | spike.drop)]
+#Removal of a substantial proportion of cells (> 10%) may be indicative of an overall issue with data quality
+dim(sce)
+data.frame(ByLibSize=sum(libsize.drop), ByFeature=sum(feature.drop),
+           ByMito=sum(mito.drop), BySpike=sum(spike.drop), Remaining=ncol(sce))
+
+
+#QC by PCA (Alternative)--------------------------------------------------
+fontsize <- theme(axis.text=element_text(size=12), 
+                  axis.title=element_text(size=16),
+                  plot.title = element_text(size=22))
+plotPCA(sce, pca_data_input="pdata") + fontsize +
+        ggtitle("PCA based on the quality metrics for each cell")
+
+########################################################################
+#
+#  C. Classification of cell cycle phase
+# 
+# ######################################################################
+
+anno <- select(org.Mm.eg.db, keys=rownames(sce), keytype="SYMBOL", column="ENSEMBL")
+ensembl <- anno$ENSEMBL[match(rownames(sce), anno$SYMBOL)]
+assignments <- cyclone(sce, mm.pairs, gene.names=ensembl)
+plot(assignments$score$G1, assignments$score$G2M, xlab="G1 score", ylab="G2/M score", pch=16)
+
+
+
+
+
+
+
+
+
+
 
 
 
